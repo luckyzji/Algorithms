d142b90ed35a03ce2dcf8808c5ce31391a7422b4
luckyzji <1978907083@qq.com>
2020-07-23 16:22:10 +0800

    增加测试类Main

---
+++ src/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+import leetcode.Solution;
+
+/**
+ * @Author ZJ
+ * @Date 2020-07-23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*********算法测试******");
+        Solution solution = new Solution();
+    }
+}
